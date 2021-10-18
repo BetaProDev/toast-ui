85296baabe89befa13ffbc3ca3388fe1f254a326
BetaProDev <92747905+BetaProDev@users.noreply.github.com>
2021-10-18 15:38:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
-(function(){
-  main(`<iframe src="http://nhn.github.io/tui.image-editor/latest/examples/example03-mobile.html" style="width:100%;height:100%"></iframe>`)
-})()
+alert("Carregado!");
+main(`<iframe src="http://nhn.github.io/tui.image-editor/latest/examples/example03-mobile.html" style="width:100%;height:100%"></iframe>`);
