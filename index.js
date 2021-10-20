97c2937957122dfa6a89edd1f86c9c78b8879e6b
BetaProDev <92747905+BetaProDev@users.noreply.github.com>
2021-10-20 17:36:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 async function main(){
-  return `<iframe src="http://nhn.github.io/tui.image-editor/latest/examples/example03-mobile.html" style="width:100%;height:100%"></iframe>`;
+  return `<iframe src="https://nhn.github.io/tui.image-editor/latest/examples/example03-mobile.html" style="width:100%;height:100%"></iframe>`;
 }
